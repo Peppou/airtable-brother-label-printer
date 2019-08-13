104eb689bd89adab3e5ed1b517a09be6a708c10d
Soroush Pour <me@soroushjp.com>
2019-08-13 11:46:27 +1000

    change USB address

--- print.py
+++ print.py
@@ -5,9 +5,9 @@ import brother_ql
 from brother_ql.devicedependent import models, label_type_specs, label_sizes
 from brother_ql import BrotherQLRaster, create_label
 
-os.environ["BROTHER_QL_PRINTER"] = "/dev/usb/lp0"
+# TODO: Get the printer USB address without hard-coding.
+os.environ["BROTHER_QL_PRINTER"] = "usb://0x04f9:0x2042"
 os.environ["BROTHER_QL_MODEL"] = "QL-700"
 
 qlr = BrotherQLRaster("QL-700")
-image_path = 'testimage-29.png'
-create_label(qlr, image_path, "29", cut=True)
+create_label(qlr, 'testimage-29.png', "29", cut=True)
